f75d397985091ce2cbdee00e94a338abc0289f19
MehulKK <mehul.kabaria@gmail.com>
2019-04-10 19:08:03 +0530

    - remove .idea file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,6 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.21")
     implementation ("com.android.support:appcompat-v7:28.0.0")
-    //implementation 'com.android.support.constraint:constraint-layout:1.1.3'
     implementation ("com.android.support.constraint:constraint-layout:2.0.0-alpha4")
     testImplementation ("junit:junit:4.12")
     androidTestImplementation ("com.android.support.test:runner:1.0.2")
